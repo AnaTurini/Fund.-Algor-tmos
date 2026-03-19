f3b98d57c67357957629fd65cb0aa50e01c1321c
Ana Beatriz Moretti Turini <pitutiturini@yahoo.com>
2026-03-19 20:37:37 -0300

    Add files via upload

---
+++ lista p1/ex3.py
@@ -0,0 +1,5 @@
+x=1
+for z in range(1,3):
+    for y in range(4,2,-1):
+        x+=y+z
+print (x)
---
+++ lista p1/ex7.py
@@ -0,0 +1,4 @@
+x=float(input("Digite o valor de x"))
+y=float(input("Digite o valor de y"))
+z=42.7*x+y**2+3
+print(f'{z:.3f}')
